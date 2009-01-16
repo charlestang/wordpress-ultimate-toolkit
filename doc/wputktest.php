d0b8261a3135517c8e0ebbec01ff4d49731fd525
TangChao.ZJU <TangChao.ZJU@b74f3ee2-dd66-11dd-a37d-c9d1c40e0883>
2009-01-16 09:03:40 +0000

    I've created a better test page than last time. You should put this test page in the default theme directory, and create a new page and use this template to display the test page.

--- doc/wputktest.php
+++ doc/wputktest.php
@@ -7,34 +7,75 @@
 Template Name: WordPress Ultimate Toolkit test page
 */
 ?>
-
+<?php
+function outputdata($objects){
+?>
+<table>
+	<thead><tr>
+<?php
+	$headcol = array();
+	foreach ($objects[0] as $key => $value){
+		echo '<td>',$key,'</td>',"\n";
+		$headcol[] = $key;
+	}
+?>
+	</tr></thead>
+	<tbody>
+<?php
+	foreach($objects as $object){
+		echo '<tr>';
+		foreach($headcol as $head){
+			$val = strip_tags($object->$head);
+			$val = mb_substr($val,0,30);
+			echo '<td>',$val,'</td>',"\n";
+		}
+		echo '</tr>';
+	}
+?>
+	</tbody>
+</table>
+<?php
+}
+?>
 <?php get_header(); ?>
+<style type="text/css">
+table {
+	border:1px solid #000;
+	border-spacing:0;
+}
+table thead{
+	background-color:#bbb;
+}
+table td{
+	border:1px solid #000;
+}
+</style>
 <?php //global $wputk_querybox;?>
 <div id="content" class="widecolumn">
 <h2>WPUTK_QueryBox Object Test</h2>
 <h3>function get_recent_posts($args = '')</h3>
-<?php //var_dump($wputk_querybox->get_recent_posts());?>
+<?php outputdata($wputk_querybox->get_recent_posts());?>
 
 <h3>function get_random_posts($args = '')</h3>
-<?php var_dump($wputk_querybox->get_random_posts());?>
+<?php outputdata($wputk_querybox->get_random_posts());?>
 
 <h3>function get_related_posts($args = '')</h3>
-<?php var_dump($wputk_querybox->get_related_posts());?>
+<?php outputdata($wputk_querybox->get_related_posts());?>
 
 <h3>function get_same_classified_posts($args = '')</h3>
-<?php var_dump($wputk_querybox->get_same_classified_posts());?>
+<?php outputdata($wputk_querybox->get_same_classified_posts());?>
 
 <h3>function get_most_commented_posts($args = '')</h3>
-<?php var_dump($wputk_querybox->get_most_commented_posts());?>
+<?php outputdata($wputk_querybox->get_most_commented_posts());?>
 
 <h3>function get_recent_comments($args = '')</h3>
-<?php var_dump($wputk_querybox->get_recent_comments());?>
+<?php outputdata($wputk_querybox->get_recent_comments());?>
 
 <h3>function get_active_commentators($args = '')</h3>
-<?php var_dump($wputk_querybox->get_active_commentators());?>
+<?php outputdata($wputk_querybox->get_active_commentators());?>
 
 <h3>function get_recent_commentators($args = '')</h3>
-<?php var_dump($wputk_querybox->get_recent_commentators());?>
+<?php outputdata($wputk_querybox->get_recent_commentators());?>
 </div>
 
 <?php get_footer(); ?>
