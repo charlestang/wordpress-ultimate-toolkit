6dd4a502a06812a16f5cf08dc892c1009abdf99a
TangChao.ZJU <TangChao.ZJU@b74f3ee2-dd66-11dd-a37d-c9d1c40e0883>
2009-02-04 08:13:43 +0000

    I learned how to create a multi instances widget today. So, I tried to make the WUT Recent Posts Widget behave in multi instances way.

--- inc/widgets.php
+++ inc/widgets.php
@@ -4,91 +4,171 @@
  */
 
 function wut_widget_recent_posts_init(){
-    function wut_widget_recent_posts_body($args){
-        extract($args);
+    function wut_widget_recent_posts_body($args, $widget_args = 1){
+        extract($args, EXTR_SKIP);
+        if (is_numeric($widget_args))
+            $widget_args = array('number' => $widget_args);
+        $widget_args = wp_parse_args($widget_args, array('number' => -1));
+        extract($widget_args, EXTR_SKIP);
+        
         $options = get_option('wut-widget-recent-posts');
-        echo $before_widget, $before_title, $options['title'], $after_title;
-        echo '<ul>', wut_recent_posts(), '</ul>';
+
+        if(!isset($options[$number]))
+            return;
+        $title = $options[$number]['title'];
+        $limit = $options[$number]['limit'];
+        $offset = $options[$number]['offset'];
+        $before = $options[$number]['before'];
+        $after = $options[$number]['after'];
+        $type = $options[$number]['type'];
+
+        $tag_args = array(
+            'limit'         => $limit,           //how many items should be show
+            'offset'        => $offset,
+            'before'        => $before,
+            'after'         => $after,
+            'type'          => $type,      //'post' or 'page' or 'both'
+            'skips'         => '',          //comma seperated post_ID list
+            'none'          => '',          //tips to show when results is empty
+            'echo'          => 1
+        );
+        echo $before_widget, $before_title, $title, $after_title;
+        echo '<ul>', wut_recent_posts($tag_args), '</ul>';
         echo $after_widget;
     }
-    function wut_widget_recent_posts_control(){
-        $defaults = array(
-            'title'     => 'WUT Recent Posts',
-            'limit'     => 10,
-            'offset'    => 0,
-            'before'    => '<li>',
-            'after'     => '</li>',
-            'type'      => 'both'
-        );
+    function wut_widget_recent_posts_control($widget_args){
+        global $wp_registered_widgets;
+        static $update = false;
+        if(is_numeric($widget_args))
+            $widget_args = array('number' => $widget_args);
+
+        $widget_args = wp_parse_args($widget_args, array('number' => -1));
+
+        extract($widget_args, EXTR_SKIP);
+
         $options = get_option('wut-widget-recent-posts');
-        $options = wp_parse_args($options, $defaults);
-        if ($_POST['wut-recent-posts-submit']){
-            $options['title'] = strip_tags($_POST['wut-recent-posts-title']);
-            $options['limit'] = intval($_POST['wut-recent-posts-limit']);
-            $options['offset'] = intval($_POST['wut-recent-posts-offset']);
-            $options['before'] = stripslashes($_POST['wut-recent-posts-before']);
-            $options['after'] = stripslashes($_POST['wut-recent-posts-after']);
-            $options['type'] = $_POST['wut-recent-posts-type'];
+
+        if(!is_array($options))
+            $options = array();
+
+        if(!$update && !empty($_POST['sidebar'])){
+            $sidebar = (string) $_POST['sidebar'];
+            $sidebars_widgets = wp_get_sidebars_widgets();
+            if(isset($sidebars_widgets[$sidebar])){
+                $this_sidebar =& $sidebars_widgets[$sidebar];
+            }else{
+                $this_sidebar = array();
+            }
+            foreach ($this_sidebar as $_widget_id){
+                if ('wut_get_recent_posts_body' == $wp_registered_widgets[$_widget_id]['callback'] && isset($wp_registered_widgets[$_widget_id]['params'][0]['number'])){
+                    $widget_number = $wp_registered_widgets[$_widget_id]['params'][0]['number'];
+                    if (!in_array("wut-widget-recent-posts-$widget_number")){
+                        unset($options[$widget_number]);
+                    }
+                }
+            }
+            foreach((array)$_POST['wut-recent-posts'] as $widget_number => $posted){
+                $options_this['title'] = strip_tags(stripslashes($posted['title']));
+                $options_this['limit'] = intval($posted['limit']);
+                $options_this['offset'] = intval($posted['offset']);
+                $options_this['before'] = stripslashes($posted['before']);
+                $options_this['after'] = stripslashes($posted['after']);
+                $options_this['type'] = $posted['type'];
+                $options[$widget_number] = $options_this;
+            }
             update_option('wut-widget-recent-posts',$options);
+            $update = true;
+        }
+        if ($number == -1){
+            $title = 'WUT Recent Posts';
+            $limit = 10;
+            $offset = 0;
+            $before = '<li>';
+            $after = '</li>';
+            $type = 'both';
+            $number = '%i%';
+        }else{
+            $title = attribute_escape($options[$number]['title']);
+            $limit = $options[$number]['limit'];
+            $offset = $options[$number]['offset'];
+            $before = attribute_escape($options[$number]['before']);
+            $after = attribute_escape($options[$number]['after']);
+            $type = $options[$number]['type'];
         }
         ?>
         <table>
             <tr>
                 <td class="alignright"><?php _e('Widget title:', 'wut');?></td>
-                <td><input id="wut-recent-posts-title" name="wut-recent-posts-title" type="text" value="<?php echo htmlspecialchars(stripslashes($options['title']));?>"/></td>
+                <td><input id="wut-recent-posts-title-<?php echo $number;?>" name="wut-recent-posts[<?php echo $number;?>][title]" type="text" value="<?php echo $title;?>"/></td>
             </tr>
             <tr>
                 <td class="alignright"><?php _e('Number of posts:', 'wut');?></td>
-                <td><input id="wut-recent-posts-limit" name="wut-recent-posts-limit" type="text" value="<?php echo $options['limit'];?>" /></td>
+                <td><input id="wut-recent-posts-limit-<?php echo $number;?>" name="wut-recent-posts[<?php echo $number;?>][limit]" type="text" value="<?php echo $limit;?>" /></td>
             </tr>
             <tr>
                 <td class="alignright"><?php _e('Offset:', 'wut');?></td>
-                <td><input id="wut-recent-posts-offset" name="wut-recent-posts-offset" type="text" value="<?php echo $options['offset'];?>" /></td>
+                <td><input id="wut-recent-posts-offset-<?php echo $number;?>" name="wut-recent-posts[<?php echo $number;?>][offset]" type="text" value="<?php echo $offset;?>" /></td>
             </tr>
             <tr>
                 <td class="alignright"><?php _e('HTML tags before a item:', 'wut');?></td>
-                <td><input id="wut-recent-posts-before" name="wut-recent-posts-before" type="text" value="<?php echo htmlspecialchars($options['before']);?>" /></td>
+                <td><input id="wut-recent-posts-before-<?php echo $number;?>" name="wut-recent-posts[<?php echo $number;?>][before]" type="text" value="<?php echo $before;?>" /></td>
             </tr>
             <tr>
                 <td class="alignright"><?php _e('HTML tags after a item:', 'wut');?></td>
-                <td><input type="text" id="wut-recent-posts-after" name="wut-recent-posts-after" value="<?php echo htmlspecialchars($options['after']);?>" /></td>
+                <td><input type="text" id="wut-recent-posts-after-<?php echo $number;?>" name="wut-recent-posts[<?php echo $number;?>][after]" value="<?php echo $after;?>" /></td>
             </tr>
             <tr>
                 <td class="alignright"><?php _e('Post type to show:', 'wut');?></td>
                 <td>
-                    <p><input type="radio" id="wut-recent-posts-type" name="wut-recent-posts-type" value="both" <?php if($options['type'] == 'both') echo 'checked="checked"';?>/>both</p>
-                    <p><input type="radio" id="wut-recent-posts-type" name="wut-recent-posts-type" value="page" <?php if($options['type'] == 'page') echo 'checked="checked"';?>/>page only</p>
-                    <p><input type="radio" id="wut-recent-posts-type" name="wut-recent-posts-type" value="post" <?php if($options['type'] == 'post') echo 'checked="checked"';?>/>post only</p>
+                    <p><input type="radio" id="wut-recent-posts-type-<?php echo $number;?>" name="wut-recent-posts[<?php echo $number;?>][type]" value="both" <?php if($type == 'both') echo 'checked="checked"';?>/>both</p>
+                    <p><input type="radio" id="wut-recent-posts-type-<?php echo $number;?>" name="wut-recent-posts[<?php echo $number;?>][type]" value="page" <?php if($type == 'page') echo 'checked="checked"';?>/>page only</p>
+                    <p><input type="radio" id="wut-recent-posts-type-<?php echo $number;?>" name="wut-recent-posts[<?php echo $number;?>][type]" value="post" <?php if($type == 'post') echo 'checked="checked"';?>/>post only</p>
                 </td>
             </tr>
             <tr>
                 <td class="alignright"><?php _e('Posts to exclude:', 'wut');?></td>
-                <td><input type="text" id="wut-recent-posts-skips" name="wut-recent-posts-skips" value="" /></td>
+                <td><input type="text" id="wut-recent-posts-skips-<?php echo $number;?>" name="wut-recent-posts[<?php echo $number;?>][skips]" value="" /></td>
             </tr>
             <tr>
                 <td class="alignright"><?php _e('If the list is empty, show:','wut');?></td>
-                <td><input type="text" id="wut-recent-posts-none" name="wut-recent-posts-none" value="No recent posts" /></td>
+                <td><input type="text" id="wut-recent-posts-none-<?php echo $number;?>" name="wut-recent-posts[<?php echo $number;?>][none]" value="No recent posts" /></td>
             </tr>
         </table>
-        <input id="wut-recent-posts-submit" name="wut-recent-posts-submit" type="hidden" value="1"/>
+        <input id="wut-recent-posts-submit-<?php echo $number;?>" name="wut-recent-posts-submit-<?php echo $number;?>" type="hidden" value="1"/>
         <?php
     }
-    
+
+    if (!$options = get_option('wut-widget-recent-posts')){
+        $options = array();
+    }
     $widget_ops =  array(
         'classname'     => 'wut-widget-recent-posts',
         'description'   => __('List the recent posts and provide some advanced options', 'wut')
     );
     $control_ops = array(
         'width'     => 400,
-        'height'    => 200
+        'height'    => 200,
+        'id_base'   => 'wut-widget-recent-posts'
     );
-    $id     = 'wut-widget-recent-posts';
+
     $name   = __('WUT Recent Posts','wut');
     $widget_cb  = 'wut_widget_recent_posts_body';
     $control_cb = 'wut_widget_recent_posts_control';
 	// Register Widgets
-	wp_register_sidebar_widget($id, $name, $widget_cb, $widget_ops);
-	wp_register_widget_control($id, $name, $control_cb, $control_ops);
+    $registerd = false;
+    foreach(array_keys($options) as $o){
+        if(!isset($options[$o]['title'])){
+            continue;
+        }
+        $id = "wut-widget-recent-posts-$o";
+        $registerd = true;
+        wp_register_sidebar_widget($id, $name, $widget_cb, $widget_ops, array('number'=>$o));
+        wp_register_widget_control($id, $name, $control_cb, $control_ops, array('number'=>$o));
+    }
+	if(!$registered){
+        wp_register_sidebar_widget("wut-widget-recent-posts-1", $name, $widget_cb, $widget_ops, array('number'=>-1));
+        wp_register_widget_control("wut-widget-recent-posts-1", $name, $control_cb, $control_ops, array('number'=>-1));
+    }
 }
 
 function wut_widget_random_posts_init(){
