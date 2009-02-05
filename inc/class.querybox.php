1c465366b5aadf17072c9577c079efa0bcb70d77
TangChao.ZJU <TangChao.ZJU@b74f3ee2-dd66-11dd-a37d-c9d1c40e0883>
2009-02-05 19:19:31 +0000

    I've completed the version 1.0 of the whole query box.

--- inc/class.querybox.php
+++ inc/class.querybox.php
@@ -16,7 +16,7 @@ class WUT_QueryBox{
         $defaults = array(
             'offset'    =>    0,
             'limit'     =>    10,
-            'type'      =>    'both',
+            'type'      =>    'post', //'both' or 'page' 
             'skips'     =>    '',
             'password'  =>    0, //show password protected post or not
             'orderby'   =>    'post_date'  //or 'post_modified'
@@ -49,7 +49,7 @@ class WUT_QueryBox{
         global $wpdb;
         $defaults = array(
             'limit'    =>   10,
-            'type'     =>   'both',
+            'type'     =>   'post',
             'skips'    =>   '',
             'password' =>   0
         );
@@ -83,7 +83,7 @@ class WUT_QueryBox{
             'offset'    => 0,
             'limit'     => 10,
             'postid'    => false,
-            'type'      => 'both',
+            'type'      => 'both', //@deprecated, begin from 2.7 page does not have tags or cates
             'skips'     => '',
             'leastshare'=> 1,
             'password'  => 0,
@@ -132,15 +132,21 @@ class WUT_QueryBox{
         return $wpdb->get_results($query);
     }
 
-    //TANGCHAO: this function is not normal, need to fixed
+    /**
+     * @version 1.0
+     * @author Charles
+     */
     function get_same_classified_posts($args = ''){
         global $wpdb;
         $defaults = array(
-            'offset'        =>            0,
-            'limit'         =>            10,
-            'postid'        =>            false,
-            'skips'         =>            '',
-            'type'          =>            'both'
+            'offset'        => 0,
+            'limit'         => 10,
+            'postid'        => false,
+            'skips'         => '',
+            'type'          => 'both', //@deprecated
+            'orderby'       => 'post_date', //'post_modified', 'comment_count' 'rand'
+            'order'         => 'DESC',
+            'password'      => 0
         );
 
         $r = wp_parse_args($args, $defaults);
@@ -159,17 +165,32 @@ class WUT_QueryBox{
         }
         $cat_ids = substr($cat_ids, 0, strlen($cat_ids) - 2);
 
-        $query  = "SELECT ID, post_title, post_date, comment_count, post_name
+        $posttype = $this->_post_type_clause($r['type']);
+        $skipclause = $this->_skip_clause('ID', $r['skips']);
+        $password = $this->_password_clause($r['password']);
+        if ($r['orderby'] !== 'rand'){
+            $orderby = $this->_orderby_clause($r['orderby'],$r['order']);
+            $orderby = str_replace('ORDER BY ','ORDER BY max_share, ',$orderby);
+        }else{
+            $orderby = "ORDER BY RAND()";
+        }
+        
+        $query  = "SELECT ID, post_author, post_title, post_date, post_content,
+                        post_name, post_excerpt, post_modified, comment_count,
+                        COUNT(tr.object_id) as max_share
                    FROM {$wpdb->posts}
                    INNER JOIN {$wpdb->term_relationships} AS tr
                               ON (ID = tr.object_id)
                    INNER JOIN {$wpdb->term_taxonomy} AS tt
                               ON (tr.term_taxonomy_id = tt.term_taxonomy_id)
-                   WHERE tt.taxonomy = 'category'
+                   WHERE post_status='publish' AND tt.taxonomy = 'category'
+                   {$posttype}
+                   {$skipclause}
+                   {$password}
                    AND ID <> {$r['postid']}
                    AND tt.term_id IN ({$cat_ids})
                    GROUP BY tr.object_id
-
+                   {$orderby}
                    LIMIT {$r['offset']}, {$r['limit']}";
 
         return $wpdb->get_results($query);
@@ -184,7 +205,7 @@ class WUT_QueryBox{
         $defaults = array(
             'offset'            => 0,
             'limit'             => 10,
-            'type'              => 'both',
+            'type'              => 'post',
             'skips'             => '',
             'password'          => 0,
             'days'              => 30 //use -1 to disable the time limit
@@ -275,54 +296,86 @@ class WUT_QueryBox{
         return $wpdb->get_results($query);
     }
 
+    /**
+     * @version 1.0
+     * @author Charles
+     */
     function get_active_commentators($args = ''){
         global $wpdb;
         $defaults = array(
-            'limit'         =>            10,
-            'offset'        =>            0,
-            'skipusers'     =>            ''
+            'limit'         => 10, //-1 to disable the limit
+            'offset'        => 0,
+            'skipusers'     => '',
+            'days'          => 15  //-1 to disable the days limit
         );
 
         $r = wp_parse_args($args, $defaults);
         $skipuserclause = $this->_skip_clause("comment_author", $r['skipusers']);
-        $query  = "SELECT comment_author, comment_author_url,
-                          COUNT(comment_ID) AS 'comment_total'
+        if ($r['limit'] < 0) {
+            $limit = '';
+        }else{
+            $limit = "LIMIT {$r['offset']},{$r['limit']}";
+        }
+        if ($r['days'] > 0){
+			$limit_date = current_time('timestamp') - ($r['days']*86400);
+			$limit_date = date('Y-m-d H:i:s', $limit_date);
+			$days = 'AND comment_date > "'.$limit_date.'" ';
+		}else {
+            $days = '';
+        }
+
+        $query  = "SELECT comment_author, comment_author_url,comment_author_email,
+                          COUNT(comment_ID) AS comment_total
                    FROM {$wpdb->comments}
                    WHERE comment_approved = '1'
                    {$skipuserclause}
                    AND (comment_author != '') AND (comment_type = '')
+                   {$days}
                    GROUP BY comment_author
                    ORDER BY comment_total DESC
-                   LIMIT {$r['offset']},{$r['limit']}";
-
+                   {$limit}";
         return $wpdb->get_results($query);
     }
 
+    /**
+     * @version 1.0
+     * @author Charles
+     */
     function get_recent_commentators($args = ''){
         global $wpdb;
         $defaults = array(
-            'limit'         =>            10,
-            'offset'        =>            0,
-            'skipusers'         =>            '',
-            'type'          =>            'both'
+            'limit'         => 10,
+            'offset'        => 0,
+            'skipusers'     => '',
+            'type'          => 'week'
         );
 
         $r = wp_parse_args($args,$defaults);
 
-        $posttype = $this->_post_type_clause($r['type']);
         $skipuserclause = $this->_skip_clause("comment_author", $r['skipusers']);
 
+        if ($r['type'] == 'week'){
+			$type = 'AND YEARWEEK(comment_date) = YEARWEEK(NOW())';
+		}elseif ($r['type'] == 'month'){
+			$type = 'AND (MONTH(comment_date) = MONTH(NOW()) AND YEAR(comment_date) = YEAR(NOW()))';
+		}else{
+            $type = '';
+        }
+        if ($r['limit'] < 0) {
+            $limit = '';
+        }else{
+            $limit = "LIMIT {$r['offset']},{$r['limit']}";
+        }
         $query  = "SELECT comment_author, comment_author_url,
                           COUNT(comment_ID) AS 'comment_total'
                    FROM {$wpdb->comments}
                    WHERE comment_approved = '1'
-                   {$r['skips']}
                    AND (comment_author != '') AND (comment_type = '')
-                   {$posttype}
                    {$skipuserclause}
+                   {$type}
                    GROUP BY comment_author
                    ORDER BY comment_total DESC
-                   LIMIT {$r['offset']},{$r['limit']}";
+                   {$limit}";
         return $wpdb->get_results($query);
     }
 
