7e685fc55be627e7671c9f8f899c139ce66b1cd1
TangChao.ZJU <TangChao.ZJU@b74f3ee2-dd66-11dd-a37d-c9d1c40e0883>
2009-02-19 04:29:05 +0000

    Most Commented Posts Widget Completed and pass a very simple test.

--- inc/widgets.php
+++ inc/widgets.php
@@ -783,14 +783,192 @@ function wut_widget_posts_by_category_init(){
 
 function wut_widget_most_commented_posts_init(){
     function wut_widget_most_commented_posts_body($args, $widget_args){
-        extract($args);
-        $options = get_option('wut-widget-recent-posts');
-        echo $before_widget, $before_title, $options['title'], $after_title;
-        echo '<ul>', wut_recent_posts(), '</ul>';
+        extract($args, EXTR_SKIP);
+        if (is_numeric($widget_args))
+            $widget_args = array('number' => $widget_args);
+        $widget_args = wp_parse_args($widget_args, array('number' => -1));
+        extract($widget_args, EXTR_SKIP);
+
+        $options = get_option('wut-widget-most-commented-posts');
+
+        if(!isset($options[$number]))
+            return;
+        $title = $options[$number]['title'];
+
+        $tag_args = array(
+            'limit'         => $options[$number]['limit'],
+            'before'        => $options[$number]['before'],
+            'after'         => $options[$number]['after'],
+            'type'          => $options[$number]['type'],
+            'skips'         => $options[$number]['skips'],
+            'days'          => $options[$number]['days'],
+            'none'          => $options[$number]['none'],
+            'password'      => $options[$number]['password'],
+            'xformat'       => $options[$number]['xformat']
+        );
+
+        echo $before_widget, $before_title, $title, $after_title;
+        echo '<ul>', wut_most_commented_posts($tag_args), '</ul>';
         echo $after_widget;
     }
     function wut_widget_most_commented_posts_control($widget_args){
+        global $wp_registered_widgets;
+        static $update = false;
+        if(is_numeric($widget_args))
+            $widget_args = array('number' => $widget_args);
 
+        $widget_args = wp_parse_args($widget_args, array('number' => -1));
+
+        extract($widget_args, EXTR_SKIP);
+
+        $options = get_option('wut-widget-most-commented-posts');
+
+        if(!is_array($options))
+            $options = array();
+
+        if(!$update && !empty($_POST['sidebar'])){
+            $sidebar = (string) $_POST['sidebar'];
+            $sidebars_widgets = wp_get_sidebars_widgets();
+            if(isset($sidebars_widgets[$sidebar])){
+                $this_sidebar =& $sidebars_widgets[$sidebar];
+            }else{
+                $this_sidebar = array();
+            }
+            foreach ($this_sidebar as $_widget_id){
+                if ('wut_widget_most_commented_posts_body' == $wp_registered_widgets[$_widget_id]['callback'] && isset($wp_registered_widgets[$_widget_id]['params'][0]['number'])){
+                    $widget_number = $wp_registered_widgets[$_widget_id]['params'][0]['number'];
+                    if (!in_array("wut-widget-most-commented-posts-$widget_number",$_POST['widget-id'])){
+                        unset($options[$widget_number]);
+                    }
+                }
+            }
+
+            foreach((array)$_POST['wut-most-commented-posts'] as $widget_number => $posted){
+                $options_this['title'] = strip_tags(stripslashes($posted['title']));
+                $options_this['limit'] = intval($posted['limit']);
+                $options_this['before'] = stripslashes($posted['before']);
+                $options_this['after'] = stripslashes($posted['after']);
+                $options_this['type'] = $posted['type'];
+                $options_this['skips'] = $posted['skips'];
+                $options_this['days'] = intval($posted['days']);
+                $options_this['none'] = strip_tags(stripslashes($posted['none']));
+                $options_this['password'] = $posted['password'];
+                $options_this['xformat'] = stripcslashes($posted['xformat']);
+                $options[$widget_number] = $options_this;
+            }
+            update_option('wut-widget-most-commented-posts',$options);
+            $update = true;
+        }
+        if ($number == -1){
+            $title = 'WUT Most Commented Posts';
+            $limit = 10;
+            $before = '<li>';
+            $after = '</li>';
+            $type = 'both';
+            $skips = '';
+            $days = 30;
+            $none = 'No Posts.';
+            $password = 'hide';
+            $xfomat = '<a href="%permalink%" title="View:%title%(Posted on %postdate%)">%title%</a>(%commentcount%)';
+            $number = '%i%';
+        }else{
+            $title = attribute_escape($options[$number]['title']);
+            $limit = $options[$number]['limit'];
+            $before = attribute_escape($options[$number]['before']);
+            $after = attribute_escape($options[$number]['after']);
+            $type = $options[$number]['type'];
+            $skips = $options[$number]['skips'];
+            $days = $options[$number]['days'];
+            $none = attribute_escape($options[$number]['none']);
+            $password = $options[$number]['password'];
+            $xformat = attribute_escape($options[$number]['xformat']);
+        }
+        ?>
+        <table>
+            <tbody>
+                <tr>
+                    <td class="alignright"><?php _e('Widget title:', 'wut');?></td>
+                    <td><input id="wut-most-commented-posts-title-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][title]" type="text" value="<?php echo $title;?>"/></td>
+                </tr>
+                <tr>
+                    <td class="alignright"><?php _e('Number of posts:', 'wut');?></td>
+                    <td><input id="wut-most-commented-posts-limit-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][limit]" type="text" value="<?php echo $limit;?>" /></td>
+                </tr>
+                <tr>
+                    <td class="alignright"><?php _e('HTML tags before a item:', 'wut');?></td>
+                    <td><input id="wut-most-commented-posts-before-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][before]" type="text" value="<?php echo $before;?>" /></td>
+                </tr>
+                <tr>
+                    <td class="alignright"><?php _e('HTML tags after a item:', 'wut');?></td>
+                    <td><input type="text" id="wut-most-commented-posts-after-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][after]" value="<?php echo $after;?>" /></td>
+                </tr>
+                <tr>
+                    <td class="alignright"><?php _e('Post type to show:', 'wut');?></td>
+                    <td>
+                        <p><input type="radio" id="wut-most-commented-posts-type-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][type]" value="both" <?php if($type == 'both') echo 'checked="checked"';?>/>both</p>
+                        <p><input type="radio" id="wut-most-commented-posts-type-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][type]" value="page" <?php if($type == 'page') echo 'checked="checked"';?>/>page only</p>
+                        <p><input type="radio" id="wut-most-commented-posts-type-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][type]" value="post" <?php if($type == 'post') echo 'checked="checked"';?>/>post only</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="alignright"><?php _e('Posts to exclude:', 'wut');?></td>
+                    <td><input type="text" id="wut-most-commented-posts-skips-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][skips]" value="" /></td>
+                </tr>
+                <tr>
+                    <td class="alignright"><?php _e('Time Limit:', 'wut');?></td>
+                    <td><?php _e('In','wut');?><input id="wut-most-commented-posts-days-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][days]" type="text" value="<?php echo $days;?>" size="7" /><?php _e('days.','wut');?></td>
+                </tr>
+                <tr>
+                    <td class="alignright"><?php _e('If the list is empty, show:','wut');?></td>
+                    <td><input type="text" id="wut-most-commented-posts-none-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][none]" value="No recent posts" /></td>
+                </tr>
+                <tr>
+                    <td class="alignright"><?php _e('Show password protected post?','wut');?></td>
+                    <td>
+                        <p><input type="radio" id="wut-most-commented-posts-password-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][password]" value="show" <?php if($password == 'show') echo 'checked="checked"';?> />Show.</p>
+                        <p><input type="radio" id="wut-most-commented-posts-password-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][password]" value="hide" <?php if($password == 'hide') echo 'checked="checked"';?> />Hide.</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="alignright"><?php _e('The list item format:', 'wut');?></td>
+                    <td><input id="wut-most-commented-posts-xformat-<?php echo $number;?>" name="wut-most-commented-posts[<?php echo $number;?>][xformat]" type="text" value="<?php echo $xformat;?>" /></td>
+                </tr>
+            </tbody>
+        </table>
+        <input id="wut-most-commented-posts-submit-<?php echo $number;?>" name="wut-most-commented-posts-submit-<?php echo $number;?>" type="hidden" value="1"/>
+        <?php
+    }
+    $options = get_option('wut-widget-most-commented-posts');
+    if(!$options){
+        $options = array();
+    }
+
+    $widget_ops =  array(
+        'classname'     => 'wut-widget-most-commented-posts',
+        'description'   => __( 'List posts order by the number of comments.', 'wut')
+    );
+    $control_ops = array(
+        'width'     => 400,
+        'height'    => 200,
+        'id_base'   => 'wut-widget-most-commented-posts'
+    );
+    $name   = __('WUT Most Commented Posts','wut');
+    $widget_cb  = 'wut_widget_most_commented_posts_body';
+    $control_cb = 'wut_widget_most_commented_posts_control';
+    // Register Widgets
+    $registered = false;
+    foreach(array_keys($options) as $o){
+        if(!isset($options[$o]['title'])){
+            continue;
+        }
+        $id = "wut-widget-most-commented-posts-$o";
+        $registered = true;
+        wp_register_sidebar_widget($id, $name, $widget_cb, $widget_ops, array('number'=>$o));
+        wp_register_widget_control($id, $name, $control_cb, $control_ops, array('number'=>$o));
+    }
+    if (!$registered){
+        wp_register_sidebar_widget("wut-widget-most-commented-posts-1", $name, $widget_cb, $widget_ops, array('number'=>-1));
+        wp_register_widget_control("wut-widget-most-commented-posts-1", $name, $control_cb, $control_ops,array('number'=>-1));
     }
 }
 
