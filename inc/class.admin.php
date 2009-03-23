113f0d65335c090be8cb6d39337fb95179584575
TangChao.ZJU <TangChao.ZJU@b74f3ee2-dd66-11dd-a37d-c9d1c40e0883>
2009-03-23 14:30:08 +0000

    Fixed the admin menu bug in WP 2.5.1

--- inc/class.admin.php
+++ inc/class.admin.php
@@ -9,7 +9,7 @@ class WUT_Admin{
     }
 
     function add_menu_items(){
-        add_menu_page(__("WordPress Ultimate Toolkit Options"), __("WUT Options"),8,__FILE__);
+        add_menu_page(__("WordPress Ultimate Toolkit Options"), __("WUT Options"),8,__FILE__, array(&$this, "hide_widgets"));
         add_submenu_page(__FILE__, __("Hide Widgets"), __("Hide Widgets"), 8, __FILE__, array(&$this, "hide_widgets"));
         add_submenu_page(__FILE__, __("Excerpt Options"),__("Excerpt Options"), 8, "wut_admin_excerpt_options", array(&$this, "excerpt_options"));
         add_submenu_page(__FILE__, __("Hide Pages"), __("Hide Pages"), 8, "wut_admin_hide_pages", array(&$this, "hide_pages"));
